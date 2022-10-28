8af0fc2ce3633ddb0c7c53867d994e3b8b9e1602
Ekatche <atchokatche@gmail.com>
2022-10-28 13:03:28 +0200

    add condition and test for blank email address

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -30,3 +30,8 @@ class ModelTests(TestCase):
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, 'sample123')
             self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """ test that creating user without an email raises a valueError"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'Test123')
