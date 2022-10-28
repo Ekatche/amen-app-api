8c559003520319ecc618f4f56fcd81ecf7c38817
Ekatche <atchokatche@gmail.com>
2022-10-28 14:09:04 +0200

    customized user admin page

--- app/core/admin.py
+++ app/core/admin.py
@@ -4,14 +4,37 @@ Django admin customisations
 
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 from . import models
 
 
 class UserAdmin(BaseUserAdmin):
-    """ define the admin page for user """
+    """define the admin page for user"""
+
     ordering = ["id"]
-    list_display = ['email', 'name']
+    list_display = ["email", "name"]
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                )
+            }
+        ),
+        (
+            _('Imporant dates'),
+            {
+                "fields": (
+                    'last_login',
+                )
+            }
+        )
+    )
+    readonly_fields = ['last_login']
 
 
 admin.site.register(models.User, UserAdmin)
-
