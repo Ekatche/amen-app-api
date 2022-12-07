adfd70a6ae3ed4e38775b246fd7099177312655e
Ekatche <71089627+Ekatche@users.noreply.github.com>
2022-12-07 12:23:27 +0100

    Updated inventory model to be of internal use only

--- app/inventory/urls.py
+++ app/inventory/urls.py
@@ -1,8 +1,8 @@
 from django.urls import include, path
 from rest_framework import routers
-from .views import InventoryViewSet
+from .views import InventoryBackofficeViewSet
 
 router = routers.DefaultRouter()
-router.register("inventory", InventoryViewSet)
+router.register("inventory", InventoryBackofficeViewSet)
 app_name = "inventory"
 urlpatterns = [path("", include(router.urls))]
