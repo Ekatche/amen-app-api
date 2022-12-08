3e71555714fda871a205dfb7ccb2be703230d9d3
Ekatche <71089627+Ekatche@users.noreply.github.com>
2022-12-08 22:02:17 +0100

    display default gateway

--- app/config/settings.py
+++ app/config/settings.py
@@ -184,7 +184,7 @@ if ENVIRONMENT.lower() in ["local"]:
     }
 else:
     ELASTICSEARCH_DSL = {
-        "default": {"hosts": "10.1.0.1:9200"},
+        "default": {"hosts": "10.1.0.1:search"},
     }
 
 CELERY_BROKER_URL = "redis://redis:6379"
